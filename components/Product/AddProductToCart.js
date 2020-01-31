78b10a48b3d1c21c5b1ba3e4c1276051371faaf0
Binu <kbinu42@gmail.com>
2020-01-31 20:51:48 +0530

    displaying individual product

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,5 +1,30 @@
+import { Input } from 'semantic-ui-react'
+
 function AddProductToCart() {
-  return <>AddProductToCart</>;
+  return <>
+    <Input
+      type='number'
+      min='1'
+      value='1'
+      placeholder="Enter Quantity"
+      action={{
+        color: "green",
+        content: "Add to Cart",
+        icon: "plus cart"
+      }}
+    />
+
+    {/* <Input
+      type="number"
+      min='1'
+      icon='plus cart'
+      iconPosition='left'
+      label={{ tag: true, content: 'Add to Cart', color: 'green' }}
+      labelPosition='right'
+
+      placeholder='Enter Quantity'
+    /> */}
+  </>;
 }
 
 export default AddProductToCart;
--- pages/product.js
+++ pages/product.js
@@ -1,12 +1,18 @@
 import axios from 'axios'
+import ProductAttributes from '../components/Product/ProductAttributes'
+import ProductSummary from '../components/Product/ProductSummary'
+import baseUrl from '../utils/baseUrl'
 
 function Product({ product }) {
   console.log(product)
-  return <>product</>;
+  return <>
+    <ProductSummary {...product} />
+    <ProductAttributes {...product} />
+  </>;
 }
 
 Product.getInitialProps = async ({ query: { _id } }) => {
-  const url = 'http://localhost:3000/api/product';
+  const url = `${baseUrl}/api/product`;
   const payload = { params: { _id } }
   const response = await axios.get(url, payload);
   return { product: response.data }
